45c04be3f963f270e5069e3d05c971e51ab27347
Lorenzo Menghini <lollomenghini@gmail.com>
2021-11-05 22:37:37 +0100

    Created complete export library

--- lib/lml.dart
+++ lib/lml.dart
@@ -1,6 +1,19 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library lml;
 
-// TODO: Export any libraries intended for clients of this package.
+export 'src/timewrappers/date.dart';
+export 'src/timewrappers/time_in_day.dart';
+export 'src/timewrappers/time_wrapper.dart';
+export 'src/timewrappers/timestamp.dart';
+export 'src/timewrappers/weekday.dart';
+
+export 'src/timewrappers/intervals/date_interval.dart';
+export 'src/timewrappers/intervals/time_in_day_interval.dart';
+export 'src/timewrappers/intervals/time_interval_wrapper.dart';
+export 'src/timewrappers/intervals/timestamp_interval.dart';
+
+export 'src/collections/iterable_utilities.dart';
+export 'src/collections/tree.dart';
+
+export 'src/identifier/identifier.dart';
+export 'src/response/error/error_details.dart';
+export 'src/response/response.dart';
